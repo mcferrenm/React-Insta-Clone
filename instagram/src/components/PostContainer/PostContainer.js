c2fceb931d947c3a96b135ac47460707c30db1e7
mcferrenm <mcferrenmax@gmail.com>
2019-01-14 16:45:18 -0500

    Check PropTypes on PostContainer component

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,7 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const PostContainer = props => {
   return <div>{props.post.username}</div>;
+};
+
+PostContainer.propTypes = {
+  post: PropTypes.shape({
+    username: PropTypes.string,
+    thumbnailUrl: PropTypes.string,
+    imageUrl: PropTypes.string,
+    like: PropTypes.number,
+    timestamp: PropTypes.string,
+    comments: PropTypes.arrayOf(
+      PropTypes.shape({
+        username: PropTypes.string,
+        text: PropTypes.string
+      })
+    )
+  })
 };
 
 export default PostContainer;
