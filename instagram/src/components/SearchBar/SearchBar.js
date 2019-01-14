1e68ce97d98460389724b4cc315482422807101b
mcferrenm <mcferrenmax@gmail.com>
2019-01-14 17:39:38 -0500

    Render all search bar elements, unstyled

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import logo from "../../img/ig-logo.png";
+
 const SearchBar = props => {
-  return <div>test</div>;
+  return (
+    <div className="search-bar">
+      <div className="left">
+        <i class="fas fa-camera" />
+        <img src={logo} alt="Instagram Logo" className="logo" />
+      </div>
+      <input type="text" placeholder="Search" />
+      <div className="right">
+        <i class="far fa-compass" />
+        <i className="far fa-heart" />
+        <i class="far fa-user" />
+      </div>
+    </div>
+  );
 };
 
 export default SearchBar;
