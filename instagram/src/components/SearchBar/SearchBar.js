f005d78d3fcca3fa770eb337ac38906124e98627
mcferrenm <mcferrenmax@gmail.com>
2019-01-17 15:31:04 -0500

    Create a Header styled-component.header that wraps SearchBar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,15 +1,82 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 import "./SearchBar.css";
 import logo from "../../img/ig-logo.png";
 
+const Header = styled.header`
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+  background: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 3rem;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+
+  i {
+    font-size: 2rem;
+  }
+
+  .left {
+    width: 33%;
+    display: flex;
+    align-items: center;
+
+    i {
+      border-right: 1px solid lightgrey;
+      padding-right: 2rem;
+      margin-right: 2rem;
+    }
+  }
+
+  .center {
+    width: 33%;
+    display: flex;
+    justify-content: center;
+
+    input {
+      width: 66%;
+      border-radius: 3px;
+      border: 1px solid lightgrey;
+      padding: 0.5rem;
+    }
+  }
+
+  .right {
+    width: 33%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    i {
+      margin-left: 3rem;
+    }
+
+    .fa-door-open {
+      cursor: pointer;
+    }
+  }
+`;
+
+const LogoHeader = styled.div``;
+
+const LogoImage = styled.img`
+  width: 150px;
+`;
+
 const SearchBar = props => {
   return (
-    <div className="search-bar">
+    <Header>
       <div className="left">
         <i className="fas fa-camera" />
-        <img src={logo} alt="Instagram Logo" className="logo" />
+        <LogoHeader>
+          <LogoImage src={logo} alt="Instagram Logo" />
+        </LogoHeader>
       </div>
       <div className="center">
         <input
@@ -27,7 +94,7 @@ const SearchBar = props => {
         <i className="far fa-user" />
         <i className="fas fa-door-open" onClick={props.handleLogout} />
       </div>
-    </div>
+    </Header>
   );
 };
 
